60773070f362f0ad79e824fb4738bcad2c10781b
ZoranNik91 <42346080+ZoranNik91@users.noreply.github.com>
2019-04-04 12:29:48 +0200

    Update AddNewEmployees.py

--- AddNewEmployees.py
+++ AddNewEmployees.py
@@ -1,7 +1,7 @@
 from datetime import datetime,date  #python library which contains date and tine methods
 import os                           #python library which contains file and folder searching methods
 
-#______PROBLEM_______--> problem je u count ako ručno unesem iste emaile onda count broji i sve radi ok ako je u pitanju isti mailovi, međutim ako unesem novi mail izbaci error , u suprotnom ako stavim count = 0 na početku preskače if petlju i uvijek je count = 0 ???????????
+
 class Employee:
 
     def Name(self,name,lastname,pay,opis):
@@ -24,7 +24,6 @@ class Employee:
                 line = sentence.split()
                 for each in line:
                     line2 = each.lower()
-                    #line2 = line2.strip("@gmail.com") #removes @gmail.com from created email to search all same emails // can work without strip("gmail.com") <----- OVDJE JE PROBLEM LEŽAO
                     line2 = ''.join([i for i in line2 if not i.isdigit()]) #remove numbers from the name.lastname1 examle -> name.lastname to compare same name.lastname in files
                     if lower == line2:  
                         count += 1
@@ -50,11 +49,10 @@ while 1:
     employee1.Name(name1,lastname1,pay1,opis)
     employee2 = Employee()
     
-    path = 'D:\\Y\zo_c++\Python'
+    path = 'C:\\users\user_name\Desktop\Python'  #<-------- PUT YOUR DIRECTORY PATH HERE
     x = os.listdir(path)
     for file in x:
         if file.startswith("Employee") and file.endswith(".txt"): # startswith() is method which recognize all string or files start with the same starting name, and endswith() is method which recognize the end of string or file name with the same ending name (word or letter)
-           #for petlja ovdj i ispod z = --- dodaj employee.CheckAllEmails
            z = os.path.join(path,file)
     zz = "Employee_2019-04-04.txt"
     employee2.CheckAllEmails(zz, [employee1.email],0) 
@@ -82,8 +80,3 @@ while 1:
 
 
    
-''' 1. napravi program u kojem mozes unositi beskonačan broj korisnika dok ne napišeš quit onda se pohrane korisnici u text file -DONE
-    2. ako se stvore korisnici s istim imenom i prezimenom napravi algoritam u kojem dodaješ brojeve poslije prezimena DONE''' 
-'''  napravi algoritam u kojem se korisnici pohranjuju isti datum u 1 text file koji će se zvati "Employee date 27.3.2'2019" - DONE 
-, napravi algoritam u kojem kad se isti dan doda više korisnika da ispod baze korisnika piše vrijeme kad je dodan npr. "Iva Ivić 300  added 15:33" DONE 
-4.napravi program u koji pregledava sve file textove i provjerava imali istih mailova ako da stvori novi za tog korisnika - DONE'''
